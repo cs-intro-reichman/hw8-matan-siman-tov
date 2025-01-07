7658c42c274bd7142bf35cc63aa3dfd95fbcbfc1
מתן סימן טוב <matan_siman_tov@MacBook-Air-sl-mtn.local>
2025-01-07 11:20:32 +0200

    dd

--- Network.java
+++ Network.java
@@ -112,11 +112,11 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-        String ans = "Network:\n"; 
+        String ans = "Network:";
         for (int i = 0; i < userCount; i++) {
-            ans += users[i].toString() + " \n"; 
+            ans += "\n" + users[i].toString(); 
         }
-        return ans;
+        return ans; 
     }
     
 }
--- User.java
+++ User.java
@@ -98,9 +98,9 @@
     }
     /** Returns this user's name, and the names that s/he follows. */
     public String toString() {
-        String ans = name + " -> ";
+        String ans = name + " ->";
         for (int i = 0; i < fCount; i++) {
-            ans += follows[i] + " ";
+            ans += " " + follows[i];
         }
         return ans; 
     }
